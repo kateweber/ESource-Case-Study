dcac3b1ce682d058f0880a50a5811262ae6b51a5
kate.weber <kmweber@mtu.edu>
2023-09-16 08:16:44 -0500

    Add utility2 silver layer

---
+++ processors/silver/utility2_install_der.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('/Users/kateweber/dev/ESource-Case-Study/processors')
+
+from sparkinit import *
+
+def cleanData():
+    spark = getSession()
+
+    file_path = "/Users/kateweber/dev/ESource-Case-Study/results/utility2_install_der_bronze"
+
+    transformSQL = """
+            SELECT 
+                DER_ID as der_id
+                , DER_TYPE as der_type
+                , DER_NAMEPLATE_RATING as namplate_rating
+                , DER_INTERCONNECTION_LOCATION as circuit_id
+                , 'Installed' as status
+                , to_date(null) as planned_install_date
+            FROM utility2_install_der
+        """
+
+    #todo: wrap in try/except 
+    #todo: fix null handling
+    df = spark.read.load(file_path)
+
+    #todo: write to s3
+
+    df.createOrReplaceTempView("utility2_install_der")
+
+    result = spark.sql(transformSQL)
+
+    result.write.save('/Users/kateweber/dev/ESource-Case-Study/results/utility2_install_der_silver', format='parquet', mode='overwrite')
---
+++ processors/silver/utility2_planned_der.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('/Users/kateweber/dev/ESource-Case-Study/processors')
+
+from sparkinit import *
+
+def cleanData():
+    spark = getSession()
+
+    file_path = "/Users/kateweber/dev/ESource-Case-Study/results/utility2_planned_der_bronze"
+
+    transformSQL = """
+            SELECT 
+                null as der_id
+                , DER_TYPE as der_type
+                , DER_NAMEPLATE_RATING as namplate_rating
+                , DER_INTERCONNECTION_LOCATION as circuit_id
+                , 'Planned' as status
+                , PLANNED_INSTALLATION_DATE as planned_install_date
+            FROM utility2_planned_der
+        """
+
+    #todo: wrap in try/except 
+    #todo: fix null handling
+    df = spark.read.load(file_path)
+
+    #todo: write to s3
+
+    df.createOrReplaceTempView("utility2_planned_der")
+
+    result = spark.sql(transformSQL)
+
+    result.write.save('/Users/kateweber/dev/ESource-Case-Study/results/utility2_planned_der_silver', format='parquet', mode='overwrite')
